d071cdc793e0f6898d95da665424b112223ab3d3
Zheng Zhu <s500730@emirates.com>
2016-11-26 14:10:05 +0000

    'adding 1. remove col if all elements are na. 2. sanity check for negative forecasting. 3. generate performance measure and log output for all legs'

--- Prediction90days_Yclass.R
+++ Prediction90days_Yclass.R
@@ -11,6 +11,9 @@ LRO <- LRO %>% mutate(FLT_key=paste(FLTNUM,ORGN,DSTN,sep='-'))
 LRO$FLTDATE <- as.Date(LRO$FLTDATE)
 LRO$DCP4 <- as.numeric(LRO$DCP4)
 
+countna <- function(x){
+  sum(is.na(x))
+}
 
 gbmpred90_month <- function(flt1df2,fitgbm,m,flt_key,DoCompare=TRUE){
   test_start_year <- max(flt1df2$year[which(flt1df2$month==m)])
@@ -18,6 +21,7 @@ gbmpred90_month <- function(flt1df2,fitgbm,m,flt_key,DoCompare=TRUE){
   datatest <- flt1df2 %>% filter(year==test_start_year,month==m)
 
   pred <- predict(gbmmodel,newdata=datatest,n.trees=500,type="response")
+  pred[which(pred<=0)] <- 1
   actual_pred <- data.frame(actual=datatest$BOOKING,date=datatest$FLTDATE,pred=round(pred))
   relerr <- with(actual_pred,mean((abs(actual-pred)/actual)))
   relerrsd <- with(actual_pred,sd((abs(actual-pred)/actual)))
@@ -50,6 +54,10 @@ gbmmodel90_month <- function(flt1df2, m){
   if(min(data$FLTDATE)<train_month){
     data <- data[which(data$FLTDATE>=train_month),]
   }
+  nacount_col <- unlist(lapply(data, countna))
+  nacol_names <- names(nacount_col)[which(nacount_col==dim(data)[1])]#remove the column with full na,i.e, some flights not fligh on particular day
+  val_cnames <- setdiff(names(data),nacol_names)
+  data <- data[,val_cnames]
   data <- data[complete.cases(data),]
   if(nrow(data)<120){
     return(-1)
@@ -67,4 +75,34 @@ gbmmodel90_month <- function(flt1df2, m){
   #               +month6+month7+month8+month9+month10+month11+month12+wdayindex+D8_D28Now+D8_D6+D8_D6_mom
   #              +booked_28days_pred+deviation,data=data,distribution = 'gaussian',n.trees = 500,interaction.depth=2, shrinkage = 0.05)
   return(fitgbm)
+}
+
+list(relerr,actual_pred,relerrsd,relerrmedian)
+flth_data <- read.csv('feat_90days_Y.csv', header = T, stringsAsFactors = FALSE)
+
+perf_agg <- NA
+perf_details <- NA
+
+index <- 1
+for(flt_key in unique(flth_data$FLTKEY)){
+  print(flt_key)
+  m <- 12
+  flth_df <- subset(flth_data, FLTKEY==flt_key)
+  flth_df$FLTDATE <- as.Date(flth_df$FLTDATE)
+  gbmmodel <- gbmmodel90_month(flth_df, m)
+  if(class(gbmmodel)!="numeric"){
+    results <- gbmpred90_month(flth_df,gbmmodel,m,flt_key,DoCompare=TRUE)
+    tmp <- data.frame(FLT_key=flt_key,month=m,relerr=results[[1]],relerrsd=results[[3]],relerrmedian=results[[4]])
+    if(index==1){
+      perf_agg <- tmp
+      perf_details <- results[[2]]
+    }else{
+      perf_agg <- bind_rows(perf_agg,tmp)
+      perf_details <- bind_rows(perf_details, results[[2]])
+    }
+  }
+  else{
+    print(paste(flt_key," not valid"))
+  }
+  index <- index + 1
 }
